45e8ad336582e25aef6c6d3882094c0eaf447524
chris-mathevet <143797375+chris-mathevet@users.noreply.github.com>
2024-09-11 10:54:44 +0200

    création des classes

--- tuto/models.py
+++ tuto/models.py
@@ -2,18 +2,16 @@ from .app import db
 
 
 class Author(db.Model):
-    def __init__(self):
-        self.id = db.Column(db.Integer, primary_key=True)
-        self.name = db.Column(db.String(100))
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
 class Book(db.Model ):
-    def __init__(self):
-        self.id = db.Column(db.Integer , primary_key =True)
-        self.price = db.Column(db.Float)
-        self.title = db.Column(db.String(100))
-        self.url = db.Column(db.String(100))
-        self.img = db.Column(db.String(100))
-        self.author_id = db.Column(db.Integer , db.ForeignKey("author.id"))
-        self.author = db.relationship("Author",backref=db.backref("books", lazy="dynamic"))
+    id = db.Column(db.Integer , primary_key =True)
+    price = db.Column(db.Float)
+    title = db.Column(db.String(100))
+    url = db.Column(db.String(100))
+    img = db.Column(db.String(100))
+    author_id = db.Column(db.Integer , db.ForeignKey("author.id"))
+    author = db.relationship("Author",backref=db.backref("books", lazy="dynamic"))
         
 def get_sample():
     return Book.query.limit(10).all()
