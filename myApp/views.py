16f9c9fd8028455d8bf8cd5d9bbd2bc2108caf16
chris-mathevet <143797375+chris-mathevet@users.noreply.github.com>
2024-10-09 16:46:42 +0200

    refactoring import et changements du next en nextPage

--- myApp/views.py
+++ myApp/views.py
@@ -1,15 +1,14 @@
 from .app import app, db
-from .models import *
-from flask import render_template, url_for, redirect
-from flask_wtf import FlaskForm
-from wtforms import StringField , HiddenField
-from wtforms. validators import DataRequired
-from wtforms import PasswordField
-from .models import User
+from . import models as mod
+
 from hashlib import sha256
-from flask_login import login_user , current_user
-from flask import request
-from flask_login import logout_user, login_required
+
+from flask import render_template, url_for, redirect,request
+from flask_wtf import FlaskForm
+from wtforms import StringField , HiddenField, PasswordField
+from wtforms. validators import DataRequired
+
+from flask_login import login_user , current_user, logout_user, login_required
 
 class AuthorForm(FlaskForm):
     id = HiddenField ("id")
@@ -20,7 +19,7 @@ class LoginForm ( FlaskForm ):
     password = PasswordField ("Password")
     next = HiddenField()
     def get_authenticated_user (self):
-        user = User.query.get(self.username.data)
+        user = mod.User.query.get(self.username.data)
         if user is None:
             return None
         m = sha256 ()
@@ -37,8 +36,8 @@ def login():
         user = f.get_authenticated_user()
         if user:
             login_user(user)
-            next = f.next.data or url_for("home")
-            return redirect(next)
+            nextPage = f.next.data or url_for("home")
+            return redirect(nextPage)
     return render_template (
         "login.html",
         form=f)
@@ -54,25 +53,25 @@ def home():
     return render_template(
         "home.html",
         title="My Books !",
-        books=get_sample())
+        books=mod.get_sample())
 
 @app.route("/detail/<id>")
 def detail(id):
     return render_template(
         "detail.html",
-        book=get_book_by_id(int(id)))
+        book=mod.get_book_by_id(int(id)))
 
 @app.route("/author/<id>")
 def one_author(id):
     return render_template(
         "author.html",
-        author=get_author_by_id(int(id)),
-        books = get_books_by_author(int(id)))
+        author = mod.get_author_by_id(int(id)),
+        books = mod.get_books_by_author(int(id)))
 
 @app.route("/edit/author/<int:id>")
 @login_required
 def edit_author(id):
-    a = get_author_by_id(id)
+    a = mod.get_author_by_id(id)
     f = AuthorForm(id=a.id, name=a.name)
     return render_template (
         "edit-author.html",
@@ -84,13 +83,12 @@ def save_author():
     a = None
     f = AuthorForm()
     if f.validate_on_submit():
-        a = get_author_by_id(int(f.id.data))
+        a = mod.get_author_by_id(int(f.id.data))
         a.name = f.name.data
         db.session.commit()
         return redirect(url_for("one_author", id=a.id))
-    a = get_author_by_id(int(f.id.data))
+    a = mod.get_author_by_id(int(f.id.data))
     return render_template("edit-author.html", author=a, form=f)
-# add,author
 
 @app.route("/add/author/")
 @login_required
@@ -107,22 +105,8 @@ def save_new_author(new=False):
     a = None
     f = AuthorForm()
     if f.validate_on_submit():
-        a = Author(name=f.name.data)
+        a = mod.Author(name=f.name.data)
         db.session.add(a)
         db.session.commit()
         return redirect(url_for("one_author", id=a.id))
     return render_template("edit-author.html", author=a, form=f)
-
-# @app.route("/save/author/", methods =("POST",))
-# def save_author():
-#     a = None
-#     f = AuthorForm()
-#     if f.validate_on_submit():
-#         a = get_author_by_id(int(f.id.data))
-#         a.name = f.name.data
-#         db.session.commit()
-#         return redirect(url_for("one_author", id=a.id))
-#     a = get_author_by_id(int(f.id.data))
-#     return render_template (
-#         "edit-author.html",
-#         author=a, form=f)
